c09008251dc8fcf524c2f7967c000fc91332cc4f
D'Angello <dangelloa.garcian@uqvirtual.edu.co>
2023-03-06 21:11:41 -0500

    a

---
+++ src/main/java/Encriptador.java
@@ -0,0 +1,15 @@
+public class Encriptador {
+    public static void main(String[] args) {
+
+    }
+
+    public static String encriptar(String mensaje) {
+        char [] nuevo = mensaje.toCharArray();
+        //for (int i = 0; i < nuevo)
+        return "";
+    }
+
+    public static String desencriptar(String mensaje) {
+        return "";
+    }
+}
